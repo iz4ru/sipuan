f861a2bd0c800c2b6ad4710759742490da643085
iz4ru <izarubot@gmail.com>
2025-06-11 12:46:47 +0700

    ignore

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,6 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\AdminLoginController;
 use App\Http\Controllers\HomeController;
 
-# Test Page
-Route::get('500', function () {
-    return view('errors.500');
-})->name('500');
-
-Route::get('404', function () {
-    return view('errors.404');
-})->name('404');
-
 # Landing Page
 Route::get('/', [HomeController::class, 'index']);
 
